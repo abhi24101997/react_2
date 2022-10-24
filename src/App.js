6bc13b1ac585c270864d198d283bfdaa883ae0fe
Abhishek <abhi24101997@gmail.com>
2022-10-24 17:20:00 +0530

    react file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import  './home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="c1">
+   {/* <home /> */}
+    <header>
+    <h1>Night <span>Beach</span> </h1>
+    <p>Tempelate By : Abhishek</p>
+    </header>
+      <nav>
+        <ul>
+          <li><a href="home.js" target="_blank">Home</a> </li>
+          <li><a href="product.jsx">About</a></li>
+          <li><a href="home.js">Products</a></li>
+          <li><a href="home.js">Service</a></li>
+          <li><a href="home.js">Design</a></li>
+          <li><a href="home.js">Contact</a></li>
+        </ul>
+      </nav>
+      <section>
+        <h4>You may use this template in any manner you like. All I ask is that you
+        leave the link back to my site at the bottom of the page.</h4>
+        <br />
+      <h5>Template Notes</h5>
+      <p>
+        The main image can be changed by either replacing the current image with
+        another one of the same size (900x402), or using a new one of what ever
+        dimensions you'd like. If you choose the latter, you must open up
+        style.css and change the dimensions of home.jsmainpic, as well as the file
+        name if that is different. If you would like to move the heading around
+        in the above image, find "home.jsmainpic h1" in style.css and modify it's
+        "left" and "top" properties, this is also true for the h2 tag.</p>
+        <br />
+        <h5>More information</h5>
+      <p>
+        I decided to leave the content portion open for the templates users to
+        do as they wish with a blank canvas. I don't like to restrict my users
+        too much, and for this reason I leave the defining of any content
+        related styles to you.
+      </p>
+      <br />
+      <h5>Template Notes</h5>
+      <p>
+        The main image can be changed by either replacing the current image with
+        another one of the same size (900x402), or using a new one of what ever
+        dimensions you'd like. If you choose the latter, you must open up
+        style.css and change the dimensions of home.jsmainpic, as well as the file
+        name if that is different. If you would like to move the heading around
+        in the above image, find "home.jsmainpic h1" in style.css and modify it's
+        "left" and "top" properties, this is also true for the h2 tag.
+      </p>
+      <br />
+      <h5>More information</h5>
+      <p>
+        I decided to leave the content portion open for the templates users to
+        do as they wish with a blank canvas. I don't like to restrict my users
+        too much, and for this reason I leave the defining of any content
+        related styles to you.
+      </p>
+      <br />
+      </section>
     </div>
   );
 }
